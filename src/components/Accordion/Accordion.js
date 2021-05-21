31fb781a144c2e26765d8b9f77d6aba72cd08b19
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2021-05-22 01:13:52 +0300

    implemented accordion component

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Accordion.css";
 
 function Accordion(props) {
   const { data } = props;
+  const [ bItemsAreToggled, setItemsAreToggled ] = useState(() => {
+    let obj = {}
+    for (let i = 0; i < data.length; i++) {
+      let key = data[i].id;
+      console.log(key);
+      obj[key] = false; 
+      console.log(data[i])
+    }
+    return obj;
+  });
+
+  const toggleItem = (id) => {
+    setItemsAreToggled((prevState) => {
+      let currentChange = !prevState[id];
+      let newState = { ...prevState }
+      for (let key in newState) { newState[key] = false; }
+      newState[id] = currentChange;
+      return newState;
+    })
+  }
 
   return (
     <div className="Accordion">
     <h1>Frequently Asked Questions</h1>
     <ul className="Accordion__container">
-      {data.map((item, idx) => (
-        <li className="Accordion__item" key={idx}>
-          <button className="Accordion__btn">{item.question}
-            <svg id="thin-x" viewBox="0 0 26 26" class="Accordion__svg-x closed" focusable="true">
+      {data.map((item) => (
+        <li className="Accordion__item" key={item.id}>
+          <button 
+            className="Accordion__btn"
+            onClick={() => toggleItem(item.id)}
+          >
+            {item.question}
+            <svg viewBox="0 0 26 26" className={`Accordion__svg-x ${bItemsAreToggled[item.id] === true ? "open" : "closed"}`} focusable="true">
               <path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path>
             </svg>
           </button>
-          <div className="Accordion__panel">
+          <div className={`Accordion__panel ${bItemsAreToggled[item.id] === true ? "open" : "closed"}`}>
             <p>{item.answer}</p>
           </div>
         </li>
