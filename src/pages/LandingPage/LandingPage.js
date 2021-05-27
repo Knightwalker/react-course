d8ae41bb414f73c6fd12979469638a81ef8e56e1
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2021-05-28 01:28:09 +0300

    login scaffolding

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -21,13 +21,11 @@ function LandingPage() {
 
         <div className="LandingPage__hero">
           <div className="LandingPage__gradient"></div>
-          <div className="LandingPage__hero-text">
-            <div>
+          <div className="LandingPage__hero-mid">
               <h1>Unlimited movies, TV shows, and more.</h1>
               <h2>Watch anywhere. Cancel at any time.</h2>
               <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
               <CallToActionForm onSubmit={handleSubmit} />
-            </div>
           </div>
         </div>
 
--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -8,12 +8,21 @@ function LoginPage() {
       <div className="LoginPage">
         <div className="LandingPage__hero">
           <div className="LandingPage__gradient"></div>
-          <div className="LandingPage__hero-text">
-            <div>
-              <h1>Unlimited movies, TV shows, and more.</h1>
-              <h2>Watch anywhere. Cancel at any time.</h2>
-              <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+          <div className="LandingPage__hero-mid">
+             
+            <div className="login-form__wrapper">
+              <h1 className="login-form__h1">Sign In</h1>
+              <form className="login-form">
+                <input className="login-form__input" placeholder="Email" type="email" />
+                <input className="login-form__input" placeholder="Password" type="password" />
+                <button className="login-form__btn" type="submit">Sing In</button>
+              </form>
+              <h4 className="login-form__h4">
+                <span className="login-form__gray">New to Netflix?</span> 
+                <span className="login-form__sign-up">Sign Up Now.</span>
+              </h4>
             </div>
+
           </div>
         </div>
       </div>
