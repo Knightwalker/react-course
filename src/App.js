cca3eaf24f6802d752feec56e73f516d272ee576
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2021-06-08 23:24:37 +0300

    ui fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import { LandingPage, HomePage, LoginPage, LogoutPage } from "./pages";
 import AuthenticationContext from "./contexts/AuthenticationContext";
 
@@ -7,12 +7,14 @@ function App() {
   const { objUser } = useContext(AuthenticationContext);
 
   return (
-    <Switch>
-      <Route exact path="/">{objUser.bUserIsAuthenticated ? <Redirect to="/browse" /> : <LandingPage />}</Route>
-      <Route exact path="/browse">{!objUser.bUserIsAuthenticated ? <Redirect to="/" /> : <HomePage />}</Route>
-      <Route exact path="/login"><LoginPage /></Route>
-      <Route exact path="/logout"><LogoutPage /></Route>
-    </Switch>
+    <Router>
+      <Switch>
+        <Route exact path="/">{objUser.bUserIsAuthenticated ? <Redirect to="/browse" /> : <LandingPage />}</Route>
+        <Route exact path="/browse">{!objUser.bUserIsAuthenticated ? <Redirect to="/" /> : <HomePage />}</Route>
+        <Route exact path="/login"><LoginPage /></Route>
+        <Route exact path="/logout"><LogoutPage /></Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from "react-router-dom";
 import App from './App';
 import { AuthenticationContextProvider } from "./contexts/AuthenticationContext";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <AuthenticationContextProvider>
+    <AuthenticationContextProvider>
         <App />
-      </AuthenticationContextProvider>
-    </Router>
+    </AuthenticationContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
