3f2d1ec5b0d0b9232f7b6c54f32d211ca4a40d39
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2023-05-12 00:48:36 +0300

    fs: deletes old firebase netflix clone, all wanted functionality was re-created

--- netflix-clone-firebase/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as LandingPage } from "./LandingPage/LandingPage";
-export { default as HomePage } from "./HomePage/HomePage";
-export { default as LoginPage } from "./LoginPage/LoginPage";
-export { default as LogoutPage } from "./LogoutPage/LogoutPage";
